a056e188ad13806fc7ea5ba2bf75d7b6b788a2be
poonam2681985 <poonam.patel@ljinstitutes.edu.in>
2021-02-20 04:55:03 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
-function compute()
+var principal = 0;
+var interestRate = 0;
+var termOfLoan = 0;
+var amount = 0;
+
+
+function updateSlider(slideAmount)
 {
-    p = document.getElementById("principal").value;
-    
+var sliderDiv = document.getElementById("result");
+sliderDiv.innerHTML = slideAmount;
 }
-        
+
+function compute(){
+event.preventDefault();   
+  var principal = parseFloat(document.getElementById("principal").value);
+if(principal<0)
+	{
+        alert("enter positive number in amount");
+        document.getElementById("principal").focus();
+        }
+else
+{
+  var d = new Date();
+  var interestRate1 = parseFloat(document.getElementById("rate").value);
+  interestRate = interestRate1 / 100;
+  var termOfLoan = parseFloat(document.getElementById("years").value);
+  var simpleInt = principal * interestRate * termOfLoan;
+  var amount = principal + simpleInt;
+  var years = d.getFullYear() + termOfLoan;
+  document.getElementById("s1").innerHTML = "if you deposit:" + principal;
+  document.getElementById("s2").innerHTML = "At an Interest Rate of :" + interestRate1 + "%";
+  document.getElementById("s3").innerHTML = "You will receive an amount of :" + amount;
+  document.getElementById("s4").innerHTML = "In the year of :" + years;
+}
+}
+      
